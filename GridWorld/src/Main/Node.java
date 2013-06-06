c1d2c788b1f71049e5f2a0d3c95d7d9c4375bd74
Tenji Tembo <us74092@umbc.edu>
2013-06-06 11:14:39 -0400

    Addded Documentation to the Node Class

--- GridWorld/src/Main/Node.java
+++ GridWorld/src/Main/Node.java
@@ -1,5 +1,24 @@
 package Main;
 
-public class Node {
+import java.util.*;
 
+/**
+ * 
+ * @author Tenji Tembo
+ * Class Details:
+ * 	This class is designed to contain the Node for the Graph ADT
+ * Attributes:
+ * 	Name - Name of the Node
+ * 	Neighbors - HashMap of all neighbors for that particular 
+ * 	object and corresponding edge weights (Q-values)
+ */
+
+public class Node {
+	
+	private String Name;
+	
+	public Node(){
+		
+	}
+	
 }
