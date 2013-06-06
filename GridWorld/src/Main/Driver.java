b9c6f1e62cf4a9c55ba2e7b4a6eb740387e3e8c7
Tenji Tembo <us74092@umbc.edu>
2013-06-06 14:00:38 -0400

    Added Conditions to Driver Class and Graph Class
    Basic Steps Conditions for the Driver Class and Graph Class. I'll wait
    for the updated Node and Graph to begin implementing the move functions.

--- GridWorld/src/Main/Driver.java
+++ GridWorld/src/Main/Driver.java
@@ -1,13 +1,44 @@
 package Main;
 
+/**
+ * 
+ * @author Tenji Tembo
+ * Class Details:
+ * 	This is main driver class to run the simulation
+ * Attributes:
+ * 	None
+ */
+
 public class Driver {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * Step one:
+		 * Create the Graph Object
+		 */
 
+		/*
+		 * Step two:
+		 * 	for int i = 0 -> 100 (episodes/trials) ----------->
+		 * 
+		 * 		find the agent - searches the graph for the agent, returns the state/node/index
+		 * 		--store in a temp node?--
+		 * 
+		 * 		check to see if agent is at the goal state
+		 * 		graph calls isWinner() (takes in a state/node/index)
+		 * 			true? - toString(), end of trial
+		 * 			false? - 
+		 * 				do
+		 * 					call Graph.move() passes in the node/index/state
+		 * 					findAgent() - agent index
+		 * 					check isWinner() - do they match?
+		 * 						if true: print, break
+		 * 						if false: continue
+		 * 				while isAgent && isGoal != true
+		 *
+		 *	<-------------				
+		 */
 	}
 
 }
--- GridWorld/src/Main/Graph.java
+++ GridWorld/src/Main/Graph.java
@@ -1,5 +1,90 @@
 package Main;
 
-public class Graph {
+import java.util.*;
+/**
+ * 
+ * @author Robot-Subgoaling
+ * Class Details:
+ * 	This is the actual Graph API. Using the nodes, 
+ * 	it will create a graph Data Structure to be able to
+ * 	simulate the agent moving through the environment
+ * Attributes:
+ * 	environment - an Arraylist of Nodes containing all 
+ * 	the available nodes in the 
+ */
 
+public class Graph {
+	
+	private ArrayList<Node> nodes;
+	
+	public Graph(){
+		/*
+		 * 9 different nodes - hand coded?
+		 * call setNeighbors()
+		 */
+	}
+	
+	/**
+	 * move() - Moves the agent from one state to the next
+	 * by simply collecting the state from each of the it
+	 */
+	public void move(int index){
+		/*
+		 * access the node on the hashmap
+		 * looks for the smallest value in the map (behavior) - collects the node's index/state
+		 * sets the agent to false in old node, and true in the new node
+		 * print out the node
+		 */
+	}
+	
+	/**
+	 * setNeighbors() - sets up the neighborhood of all the
+	 * node elements
+	 */
+	public void setNeighbors(){
+		/*
+		 * 	for each node in nodes
+		 * 		add HashMap entries of neighboring Nodes and their corresponding Q-Values
+		 * 			note: could add random number generator for the random numbers.
+		 */
+	}
+	
+	/**
+	 * isWinner() - checks to see if agent is at goal state
+	 * @param index - index of the node we are checking
+	 * @return - true or false
+	 */
+	public boolean isWinner(int index){
+		return true;
+	}
+	
+	/**
+	 * findAgent() - scans the arraylist for the agent location
+	 * @return - index of the agent's locations
+	 */
+	public int findAgent(){
+		return 0;
+	}
+	
+	/**
+	 * toString() - returns the toString of the entire Graph
+	 * @return - tostring of the entire graph
+	 */
+	public String toString(){
+		StringBuilder str = new StringBuilder();
+		
+		for(int i = 0; i < nodes.size(); i++)
+			str.append(printNode(i));
+		
+		return str.toString();
+	}
+	
+	/**
+	 * printNode() - returns the toString of just a single node element
+	 * @param index - index of the node
+	 * @return - toString variable
+	 */
+	public String printNode(int index){
+		return nodes.get(index).toString();
+	}
 }
